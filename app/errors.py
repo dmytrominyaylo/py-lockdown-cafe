7d642d293561331d59528846a46cce42daf82a83
Dmytro Minyaylo <dimon9660@gmail.com>
2025-02-18 14:51:47 +0200

    changed 3 py-lockdown-cafe

--- app/errors.py
+++ app/errors.py
@@ -3,16 +3,24 @@ class VaccineError(Exception):
 
 
 class NotVaccinatedError(VaccineError):
-    def __init__(self, message: str = "Visitor is not vaccinated.") -> None:
+    def __init__(
+            self,
+            message: str = "Visitor is not vaccinated."
+    ) -> None:
         super().__init__(message)
 
 
 class OutdatedVaccineError(VaccineError):
-    def __init__(self, message: str = "Vaccine is outdated.") -> None:
+    def __init__(
+            self,
+            message: str = "Vaccine is outdated."
+    ) -> None:
         super().__init__(message)
 
 
 class NotWearingMaskError(Exception):
-    def __init__(self, message: str = "Visitor is not wearing a mask.") \
-            -> None:
+    def __init__(
+            self,
+            message: str = "Visitor is not wearing a mask."
+    ) -> None:
         super().__init__(message)
--- app/main.py
+++ app/main.py
@@ -1,25 +1,16 @@
 from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
-from typing import Any
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    masks_needed = sum(1 for friend in friends
-                       if try_visit_cafe(friend, cafe) == "mask")
-    all_vaccinated = all(try_visit_cafe(friend, cafe) != "vaccine"
-                         for friend in friends)
-    if not all_vaccinated:
-        return "All friends should be vaccinated"
-    if masks_needed:
-        return f"Friends should buy {masks_needed} masks"
+    masks = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            masks += 1
+    if masks > 0:
+        return f"Friends should buy {masks} masks"
     return f"Friends can go to {cafe.name}"
-
-
-def try_visit_cafe(friend: Any, cafe: Any) -> Any:
-    try:
-        cafe.visit_cafe(friend)
-        return None
-    except VaccineError:
-        return "vaccine"
-    except NotWearingMaskError:
-        return "mask"
